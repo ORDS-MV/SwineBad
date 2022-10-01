c7292dcc30c9ae4cd7d3a3ec7b9f0ad5308ffe9d
AnjaEggert <eggert@fbn-dummerstorf.de>
2022-10-01 18:35:33 +0200

    Code for Europa-shapefile added

--- plot_animation.R
+++ plot_animation.R
@@ -13,10 +13,15 @@ df %>%
   mutate(year=year(Datum))->
   data
 
-# Download data fromhttps://hub.arcgis.com/datasets/ae25571c60d94ce5b7fcbf74e27c00e0/about
+# Download data from https://hub.arcgis.com/datasets/ae25571c60d94ce5b7fcbf74e27c00e0/about
 
 germany <- readOGR("./vg2500_geo84/vg2500_bld.shp", use_iconv = TRUE, encoding = "UTF-8")
 germany_tab <- broom::tidy(germany)
+
+# Download data from https://www.efrainmaps.es/english-version/free-downloads/europe/
+
+europe <- readOGR("./europe/Europe.shp", use_iconv = TRUE, encoding = "UTF-8")
+europe_tab <- broom::tidy(europe)
 
 ggplot() +
   geom_polygon(data = germany_tab,
@@ -37,6 +42,29 @@ ggplot() +
         axis.title=element_blank(),
         panel.grid = element_blank(),
         axis.text = element_blank()) +
-  labs(title = 'Jahr: {closest_state}') ->p
+  labs(title = 'Jahr: {closest_state}') -> p.deu
 
-pp <- animate(p, renderer = gifski_renderer(file="swinemunde.gif"))
+pp.deu <- animate(p.deu, renderer = gifski_renderer(file="swinemunde.gif"))
+
+ggplot() +
+  geom_polygon(data = europe_tab,
+               aes(x = long, y = lat, group = group),
+               fill = "white",
+               colour = "gray80") +
+  geom_segment(data=data,color="#d73027",
+               aes(xend=sw_long, yend=sw_lat, x = longitude, y = latitude),
+               arrow = arrow(length = unit(3, "mm"))) + 
+  geom_point(data = data,
+             aes(x = longitude, y = latitude)) +
+  theme_minimal() +
+  coord_map() + 
+  transition_states(year) +
+  guides(color='none', size=guide_legend('# of courses')) + 
+  theme(legend.position = 'top',
+        legend.box = "vertical",
+        axis.title=element_blank(),
+        panel.grid = element_blank(),
+        axis.text = element_blank()) +
+  labs(title = 'Jahr: {closest_state}') -> p.eu
+
+pp.eu <- animate(p.eu, renderer = gifski_renderer(file="swinemunde.gif"))
