97226cc063d2c207fe63f4b774d744926a8528ef
Max Schröder <max.schroeder@uni-rostock.de>
2022-10-01 21:13:39 +0200

    Working coordinate requests.

--- get_coordinates.R
+++ get_coordinates.R
@@ -9,7 +9,7 @@ get_place_id_coords <- function(ort) {
   place_id <- cresult$features[[1]]$properties$place_id
   osm_coords <- cresult$features[[1]]$geometry$coordinates
   
-  return(place_id, osm_coords)
+  tibble(place_id=place_id, lat=osm_coords[[2]], lon=osm_coords[[1]])
 }
 
 get_wikidata_url <- function(place_id) {
@@ -18,9 +18,12 @@ get_wikidata_url <- function(place_id) {
   wikidata_id <- cresult$extratags$wikidata
   wikidata_url <- paste0("https://www.wikidata.org/wiki/", wikidata_id)
   
-  return(wikidata_url)
+  tibble(wikidata_id=wikidata_id, wikidata_url=wikidata_url)
 }
 
-# coordinates %>%
-#   rowwise() %>%
-#   summarise(place_id=get_place_id(Ort))
+coordinates <- coordinates %>%
+  as_tibble() %>% 
+  mutate(place_id_coords = pmap(list(Ort), get_place_id_coords))  %>%
+  unnest(c('place_id_coords')) %>%
+  mutate(wikidata_id_url = pmap(list(place_id), get_wikidata_url))  %>%
+  unnest(c('wikidata_id_url'))
